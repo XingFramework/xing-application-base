5fea2c6c22174183b9beb122d658b6197809891e
Evan Dorn <evan@lrdesign.com>
2014-04-24 11:55:31 -0700

    Add serializer for Study that includes ScreenerQuestions

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,2 +1,4 @@
 class Study < ActiveRecord::Base
+  has_many   :screener_questions
+  belongs_to :user
 end
---
+++ spec/serializers/study_serializer.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+
+describe StudySerializer do
+  let :study do
+    FactoryGirl.build_stubbed(
+      :study,
+      :screener_questions => [
+        FactoryGirl.build_stubbed(:screener_question_single),
+        FactoryGirl.build_stubbed(:screener_question_multiple),
+        FactoryGirl.build_stubbed(:screener_question_open)
+      ]
+    )
+  end
+
+  describe 'as_json' do
+    subject :json do
+      StudySerializer.new(study).to_json
+    end
+
+    it { should be_present }
+    it { should have_json_path('study/id') }
+    it { should have_json_path('study/title') }
+    it { should have_json_size(3).at_path("study/screener_questions") }
+    it { should have_json_type(Array).at_path('study/screener_questions')}
+    it { should have_json_type(Array).at_path('study/screener_questions/0/options')}
+  end
+end
