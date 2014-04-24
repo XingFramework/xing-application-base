0972b82ee0f193178f84761c1daa3bcc2020e1f0
Judson <nyarly@gmail.com>
2014-04-24 01:22:00 -0700

    Beginning addition of karma-ng-html2js-preprocessor

--- angular/karma/karma-unit.tpl.js
+++ angular/karma/karma-unit.tpl.js
@@ -18,9 +18,33 @@ module.exports = function ( config ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [
+      'karma-jasmine',
+      'karma-firefox-launcher',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      'karma-coffee-preprocessor'
+    ],
     preprocessors: {
       '**/*.coffee': 'coffee',
+      '**/*.html': ['ng-html2js'],
+      '**/*.json': ['ng-html2js']
+    },
+
+    ngHtml2JsPreprocessor: {
+      // strip this from the file path
+      stripPrefix: 'public/',
+      // prepend this to the
+      prependPrefix: 'served/',
+
+      // or define a custom transform function
+      //cacheIdFromPath: function(filepath) {
+      //  return cacheId;
+      //},
+
+      // setting this option will create only a single module that contains templates
+      // from all the files, so you can load them all with module('foo')
+      //moduleName: 'foo'
     },
 
     /**
