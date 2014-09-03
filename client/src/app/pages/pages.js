019874bccc4f5b2ce45b873ed19934e072c064a7
Judson <nyarly@gmail.com>
2014-09-03 11:51:42 -0700

    First steps of Pages and CMS API

---
+++ client/src/app/pages/pages.js
@@ -0,0 +1,38 @@
+import {configuration} from '../../common/config';
+
+angular.module( `${configuration.appName}.pages`, [
+  'ui.router.state',
+  'restangular'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'pages', {
+    url: '/pages/:permalink',
+    views: {
+      "main": {
+        controller: 'PagesCtrl',
+        templateUrl: 'pages/pages.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Awesome' }
+  });
+})
+
+.factory('Pages', ['Restangular', function PagesService(Restangular) {
+  return Restangular.service('pages');
+}])
+
+.controller( 'PagesCtrl', ['$scope', '$stateParams', 'Pages', '$sce',
+  function PagesController( $scope, $stateParams, Pages, $sce ) {
+    $scope.content = {};
+    Pages.one($stateParams.permalink).get().then(function(page) {
+      $scope.content = $sce.trustAsHtml(page.content);
+      $scope.headline = page.headline;
+      var metadata = {};
+      metadata.pageTitle = page.title;
+      metadata.pageKeywords = page.keywords;
+      metadata.pageDescription = page.description;
+      metadata.pageCss = page.css;
+      $scope.$emit('metadataSet', metadata);
+    });
+}]);
