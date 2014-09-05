32e5da1248f23b7758fc5085f5c2002d6d988532
Evan Dorn <evan@lrdesign.com>
2014-09-05 15:56:10 -0700

    Remove incorrect comment

--- server/spec/models/role/admin_spec.rb
+++ server/spec/models/role/admin_spec.rb
@@ -7,7 +7,6 @@ describe Role::Admin do
   describe 'abilities' do
     subject(:ability) { Ability.new(user) }
 
-    #no meal donation entries cause they depend on current user - for now
     it{ expect(ability).to be_able_to :manage, Page }
     it{ expect(ability).to be_able_to :manage, MenuItem }
     it{ expect(ability).to be_able_to :manage, Document }
