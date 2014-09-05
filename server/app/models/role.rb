23fd6ee8a2396630ec7cafa432f204074f87b7b3
Evan Dorn <evan@lrdesign.com>
2014-09-05 14:45:25 -0700

    Add basic role structure.
    [#78204760]

--- server/app/models/role.rb
+++ server/app/models/role.rb
@@ -1,7 +1,7 @@
 require 'class_registry'
 class Role
   include ClassRegistry
-  def self.registrar; Role; end
+  #def self.registrar; Role; end
 
   def self.for(user)
     registry[user.role_name].new.tap do |role|
@@ -9,10 +9,15 @@ class Role
     end
   end
 
+  def self.users
+    User.where(:role_name => registrar.registry_key(self))
+  end
+
 
   def role_name
     user.role_name
   end
+
 
   attr_accessor :user
 
