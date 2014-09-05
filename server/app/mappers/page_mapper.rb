02546017bc484ccdc1a8e607bca7e864c89f324f
Evan Dorn <evan@lrdesign.com>
2014-09-05 12:46:20 -0700

    Refactor the page json mapper.
    [#77769496]

--- server/app/mappers/page_mapper.rb
+++ server/app/mappers/page_mapper.rb
@@ -1,42 +1,41 @@
-class PageMapper
-  def initialize(json)
-    @source_json = json
-    @source_hash = JSON.parse(json).with_indifferent_access
-  end
+class PageMapper < HypermediaJSONMapper
+  attr_accessor :bad_blocks
 
   def save
-    page_attributes = @source_hash['data']
-    contents_hash   = page_attributes.delete('contents')
-    page = Page.new(page_attributes)
+    page_data     = unwrap_data(@source_hash)
+    contents_data = page_data.delete('contents')
+    page = Page.new(page_data)
     page.set_url_slug
 
-    add_contents(page, contents_hash)
+    add_contents(page, contents_data)
     unless page.save
       raise "Unable to save page.  reasons: #{page.errors.inspect}"
     end
   end
 
-  def add_contents(page, contents_hash)
-    bad_blocks = []
-    contents_hash.each do |name, body|
-      if (format = page.named_content_format(name)).present?
-        content_block = ContentBlock.new(
-          :content_type => format[:content_type],
-          :body         => body['data']['body']
-        )
-        page.page_contents << PageContent.new(
-          :name => name,
-          :content_block => content_block
-        )
-        page.sanitize(name, content_block)
-      else
-        bad_blocks << { name => body['data']['body'] }
-      end
+  def add_contents(page, contents_data)
+    self.bad_blocks = []
+    contents_data.each do |name, content_block_hash|
+      add_content_block(page, name, unwrap_data(content_block_hash))
     end
-    if bad_blocks.present?
+    if self.bad_blocks.present?
       raise BadContentException.new("JSON contained invalid content: #{bad_blocks.inspect}")
     end
   end
 
-  class BadContentException < Exception; end
+  def add_content_block(page, block_name, block_data)
+    if (format = page.named_content_format(block_name)).present?
+      page.page_contents << PageContent.new(
+        :name => block_name,
+        :content_block => ContentBlock.new(
+          :content_type => format[:content_type],
+          :body         => block_data['body']
+        )
+      )
+      page.sanitize(block_name, page.contents[block_name])
+    else
+      self.bad_blocks << { block_name => block_data['body'] }
+    end
+  end
+
 end
