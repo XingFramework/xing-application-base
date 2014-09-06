ac532ce2840a8a8fbdc150b667f1d6043b2eb084
patriciaho <patricia.p.ho@gmail.com>
2014-09-05 18:12:29 -0700

    clean up cruft

--- server/spec/controllers/pages_controller_spec.rb
+++ server/spec/controllers/pages_controller_spec.rb
@@ -29,29 +29,5 @@ describe PagesController do
       get :show, :slug => slug
       expect(assigns[:page]).to eq(page)
     end
-
-    # describe "for a non-existent page" do
-    #   it "should return status 404" do
-    #     get :show, :prefix => 'test', :permalink => "how_do_we_know_that_we_truly_exist"
-    #     response.status.should == 404
-    #   end
-    # end
-
-    # describe "for an unpublished page" do
-    #   let! :page do
-    #     FactoryGirl.create(:page, :publish_end => Time.now - 1.day)
-    #   end
-
-    #   before(:each) do
-    #     get :show,
-    #         :permalink => page.permalink
-    #   end
-    #   it "should not expose the page as @page" do
-    #     assigns[:page].should == nil
-    #   end
-    #   it "should return status 404" do
-    #     response.status.should == 404
-    #   end
-    # end
   end
 end
