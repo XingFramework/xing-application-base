e12bb002a4ef4e5be2d494f615e43e3856dbccef
Evan Dorn <evan@lrdesign.com>
2014-09-06 16:11:04 -0700

    Begun Menu system implemetation, much of Menu class is written.
    [#77823448]

--- server/app/models/menu.rb
+++ server/app/models/menu.rb
@@ -1,3 +1,19 @@
 class Menu
+  attr_accessor :menu_item
 
+  def initialize(item)
+    if item.root?
+      @menu_item = item
+    else
+      raise "Cannot instantiate Menu with a non-root MenuItem"
+    end
+  end
+
+  def self.list
+    wrap(MenuItem.where(:parent_id => nil))
+  end
+
+  def self.wrap(item_or_array)
+    [*item_or_array].map { |mi| Menu.new(mi) }
+  end
 end
--- server/spec/models/menu_spec.rb
+++ server/spec/models/menu_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe Menu do
+  let :root_1 do FactoryGirl.create(:menu_item, :parent => nil, :name => 'Menu 1') end
+  let :root_2 do FactoryGirl.create(:menu_item, :parent => nil, :name => 'Menu 2') end
+  let :child_1 do FactoryGirl.create(:menu_item, :parent => root_1, :name => 'Child 1') end
+  let :child_2 do FactoryGirl.create(:menu_item, :parent => child_1, :name => 'Child 2') end
 
   describe 'list' do
-    let! :root_1 do FactoryGirl.build(:menu_item, :parent => nil, :name => 'Menu 1') end
-    let! :root_2 do FactoryGirl.build(:menu_item, :parent => nil, :name => 'Menu 2') end
-    let! :child_1 do FactoryGirl.build(:menu_item, :parent => root_1, :name => 'Child 1') end
-    let! :child_2 do FactoryGirl.build(:menu_item, :parent => child_1, :name => 'Child 2') end
+
+    before do
+      MenuItem.delete_all
+      root_1; root_2; child_1; child_2
+    end
 
     it "should have four menu items" do
       expect(MenuItem.count).to eq(4)
@@ -18,6 +23,33 @@ describe Menu do
         expect(mm).to be_a(Menu)
       end
     end
+  end
+
+  describe 'instantiation' do
+    let :menu do
+      Menu.new(item)
+    end
+
+    describe "with a root item" do
+      let :item do root_1 end
+
+      it "should result in a Menu with that MenuItem stored" do
+        expect(menu).to           be_a(Menu)
+        expect(menu.menu_item).to eq(item)
+      end
+    end
+    describe "with a non-root item" do
+      let :item do child_1 end
+
+      it "should result in a Menu with that MenuItem stored" do
+        expect{ menu }.to raise_error
+      end
+    end
+  end
+
+  describe 'delegation' do
+    let :root_1 do FactoryGirl.create(:menu_item, :parent => nil, :name => 'Menu 1') end
+
   end
 
 end
