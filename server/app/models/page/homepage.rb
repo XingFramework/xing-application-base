0e5fe93fd920c6ecbd484f6fb86ac1cecf483027
Evan Dorn <evan@lrdesign.com>
2014-09-09 15:03:16 -0700

    Homepage record validations
    [#78492496]

--- server/app/models/page/homepage.rb
+++ server/app/models/page/homepage.rb
@@ -1,10 +1,22 @@
 class Page::Homepage < Page
   register :homepage
 
-  def initialize
-    Page::Homepage.where(:url_slug => 'homepage').first
+  validate :there_can_be_only_one
+
+  def self.get
+    Page::Homepage.first
   end
 
+  def there_can_be_only_one
+    if !self.persisted? and Page::Homepage.count > 0
+      errors[:base] << "You may not create a second homepage record."
+    end
+    if self.url_slug != 'homepage'
+      errors.add(:url_slug, "Must be the exact string 'homepage'")
+    end
+  end
+
+  #class DuplicateHomepageException < Exception; end
 
 
 end
--- server/spec/models/page/homepage_spec.rb
+++ server/spec/models/page/homepage_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 describe Page::Homepage do
 
   it "should have one created by seeds.rb" do
-    expect(Page::Homepage.new).to be_a(Page::Homepage)
-    expect(Page::Homepage.new).to be_persisted
+    expect(Page::Homepage.get).to be_a(Page::Homepage)
+    expect(Page::Homepage.get).to be_persisted
+    expect(Page::Homepage.get.url_slug).to eq('homepage')
   end
 
 
-  it "should not allow two homepages to be created in the DB"
+  it "should not allow two homepages to be created in the DB" do
+    expect(Page::Homepage.new(:url_slug => 'homepage2')).not_to be_valid
+  end
 
-  it "should not allow permalink to be changed"
+  it "should not allow permalink to be changed" do
+    page = Page::Homepage.get
+    page.url_slug = 'something_else'
+    expect(page).not_to be_valid
+  end
 
 end
