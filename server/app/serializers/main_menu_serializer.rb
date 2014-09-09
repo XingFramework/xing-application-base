08c315ab2a22269db3c77a3a1936d5271fe7ae85
Hannah Howard <hannah@hannahhoward.net>
2014-09-08 18:07:01 -0700

    Main Menu Serializer

--- server/app/serializers/main_menu_serializer.rb
+++ server/app/serializers/main_menu_serializer.rb
@@ -1,2 +1,13 @@
-class MainMenuSerializer < ActiveModel::Serializer
+class MainMenuSerializer < BaseSerializer
+
+  def as_json_without_wrap
+    JsonTreeLister.new(object, MainMenuNodeSerializer).render
+  end
+
+  def links
+    {
+      :self => routes.main_menu_path
+    }
+  end
+
 end
