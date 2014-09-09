8c1f400fd6cb96f1ad8f1b6dd0b4fc9706083b9b
patriciaho <patricia.p.ho@gmail.com>
2014-09-09 14:23:12 -0700

    pull out unnecessary methods

--- server/app/controllers/json_controller.rb
+++ server/app/controllers/json_controller.rb
@@ -8,15 +8,4 @@ class JsonController < ActionController::Base
   def parse_json
     @parsed_json ||= JSON.parse(json_body)
   end
-
-  def current_user
-    @current_user ||= set_user
-  end
-
-  private
-  def set_user
-    return unless request.headers['X-User-Id']
-    @user_id = request.headers['X-User-Id']
-    @current_user = User.find(@user_id)
-  end
 end
