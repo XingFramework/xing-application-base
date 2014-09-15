f335ead029a6c5db1612247d8e0a602e62b1367d
Evan Dorn <evan@lrdesign.com>
2014-09-15 00:11:49 -0700

    Remove unused static controller.
    [#78797510]

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -10,21 +10,14 @@ Rails.application.routes.draw do
   namespace :admin do
     #resources :images
     #resources :documents
-    #get "pages/:url_slug", :to => 'pages#show', :as => :page
-    #put "pages/:url_slug", :to => 'pages#update'
     resources :pages, :param => :url_slug
     resources :menus, :only => [ :show, :index ]
     resources :menu_items
     resources :content_blocks
 
-    resources :blog_posts, :except => 'show'
-    resources :topics, :except => 'show'
+    #resources :blog_posts, :except => 'show'
   end
 
-  resources :topics, :only => %w{show index}
-
   mount_devise_token_auth_for 'User', at: '/users'
-
-  root :to => 'static#index'
 
 end
