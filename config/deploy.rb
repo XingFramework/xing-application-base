cd0ca76b8567b8e083d6e3e44fd1c7f9c51a9fa1
Judson <nyarly@gmail.com>
2014-09-16 18:08:05 -0700

    Setting up deployment
    Conflicts:
    	Rakefile
    	backend/Capfile
    	backend/config/deploy.rb
    	backend/config/deploy/production.rb
    	backend/config/deploy/staging.rb

---
+++ config/deploy.rb
@@ -0,0 +1,60 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :repo_url, 'git@git.lrdesign.com:lrd/uccf-website.git'
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+set :linked_files, %w{server/config/database.yml server/config/secrets.yml}
+set :linked_dirs, %w{
+  client/node_modules client/vendor
+  server/log server/tmp/pids server/tmp/cache server/tmp/sockets server/vendor/bundle server/public/system
+}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :server_path, proc{ File::join(release_path, "server") }
+
+namespace :deploy do
+  desc 'Build app'
+  task :build do
+    on roles(:app), :in => :parallel do
+      within release_path do
+        with :rails_env => fetch(:stage) do
+          rake "build"
+        end
+      end
+    end
+  end
+  after 'symlink:shared', :build
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      within fetch(:server_path) do
+        execute :touch, 'tmp/restart.txt'
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+#  after :restart, :clear_cache do
+#    on roles(:web), in: :groups, limit: 3, wait: 10 do
+#      # Here we can do anything such as:
+#      within fetch(:server_path) do
+#        with :rails_env => fetch(:stage) do
+#          execute :bundle, 'exec', 'rake', 'cache:clear'
+#        end
+#      end
+#    end
+#  end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,29 @@
+server 'appserver2.lrdesign.com', user: 'root', roles: %w{web app}
+set :branch, "staging"
+set :application, "staging.uccf-losangles.org"
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
