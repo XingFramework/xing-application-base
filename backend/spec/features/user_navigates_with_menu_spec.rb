5ff79b0897d82f6781da5fddf6dd99822db33c95
Mike Wong <mikewong79@gmail.com>
2014-10-21 17:26:20 -0700

    no child link test, added to user sign up spec, timing issue
    [finishes #81131152]

--- backend/spec/features/user_navigates_with_menu_spec.rb
+++ backend/spec/features/user_navigates_with_menu_spec.rb
@@ -18,10 +18,6 @@ feature "User navigates with the main menu", :js => true, :vcr => {} do
     FactoryGirl.create(:menu_item_without_page, name: "Path Link", parent_id: 1, path: 'http://lrdesign.com')
   end
 
-  let! :child_link do
-    FactoryGirl.create(:menu_item, name: 'Child Link', parent: page_link, page: page_two)
-  end
-
   scenario "visit a path" do
     visit '/'
     click_on("Path Link")
@@ -35,16 +31,6 @@ feature "User navigates with the main menu", :js => true, :vcr => {} do
     expect(page).to have_content(page_one.contents["headline"].body)
     expect(page.body).to include(page_one.contents["main"].body)
     expect(URI(current_url).fragment).to eq("/pages/" + page_one.url_slug)
-  end
-
-  scenario "visit a child menu item" do
-    visit '/'
-    debugger
-    # debugger
-    # click_on("Child Link")
-    # expect(page).to have_content(page_two.contents["headline"].body)
-    # expect(page.body).to include(page_two.contents["main"].body)
-    # expect(URI(current_url).fragment).to eq("/pages/" + page_two.url_slug)
   end
 
 end
