77cf80f6ac57e80957d785e11d0d0352c545d873
Evan Dorn <evan@lrdesign.com>
2014-11-17 13:21:15 -0800

    Fix outgoing email config.
    [Finishes #82893192]

--- backend/config/initializers/smtp.rb
+++ backend/config/initializers/smtp.rb
@@ -1,13 +1,14 @@
-FROM_ADDRESS     = "sysadmin@equibid.com"
-REPLY_TO_ADDRESS = "sysadmin@equibid.com"
-
+byebug
+FROM_ADDRESS     = Rails.application.secrets.emails['from']
+REPLY_TO_ADDRESS = Rails.application.secrets.emails['reply_to'] ||
+                     Rails.application.secrets.emails['from']
 
 ActionMailer::Base.smtp_settings = {
-  :address              => Rails.application.secrets.smtp[:address],
-  :port                 => Rails.application.secrets.smtp[:port],
-  :domain               => Rails.application.secrets.smtp[:domain],
-  :user_name            => Rails.application.secrets.smtp[:user_name],
-  :password             => Rails.application.secrets.smtp[:password],
+  :address              => Rails.application.secrets.smtp['address'],
+  :port                 => Rails.application.secrets.smtp['port'],
+  :domain               => Rails.application.secrets.smtp['domain'],
+  :user_name            => Rails.application.secrets.smtp['user_name'],
+  :password             => Rails.application.secrets.smtp['password'],
   :authentication       => :plain,
   :raise_delivery_errors => (Rails.env.development?),
   :enable_starttls_auto => true
@@ -17,7 +18,7 @@ ActionMailer::Base.smtp_settings = {
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to      = "test@lrdesign.com"   # Set this to your email address for development!
+    message.to      = Rails.application.secrets.emails['test'] rescue "test@lrdesign.com"
   end
 end
 
