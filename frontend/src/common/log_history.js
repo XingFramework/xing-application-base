7ecbfb9e3a875a1b3c98efc054641ab2b9d5a58e
Evan Dorn <evan@lrdesign.com>
2015-01-29 18:51:45 -0800

    Clean up objects passed to log() or table() so that they can be usefully emitted to the logger.
    [#87057100]

--- frontend/src/common/log_history.js
+++ frontend/src/common/log_history.js
@@ -1,5 +1,44 @@
 // very simple console wrapper to preserve history
 (function(){
+
+  // Returns a version of obj without functions
+  // or recursive references.  Recursive references
+  // will blow out the stack when we try to
+  // serialize.
+  function strippedObject(obj, cache = []) {
+    var newObj = {};
+    for (var property in obj) {
+      if (obj.hasOwnProperty(property)) {
+        var value = obj[property];
+        switch (typeof value) {
+          case "function":
+            break;
+          case "object":
+            if (cache.indexOf(property) == -1) {
+              cache.push(property);
+              newObj[property] = strippedObject(value, cache);
+            } else {
+              newObj[property] = "<recursive object reference>";
+            }
+            break;
+          default:
+            newObj[property] = obj[property];
+        }
+      }
+    }
+    return newObj;
+  }
+  function serializable(value) {
+    switch (typeof value) {
+      case "function":
+        return "function";
+      case "object":
+        return strippedObject(value);
+      default:
+        return value;
+    }
+  }
+
   if (!console.undecorated_log) {
     console.undecorated_log = console.log;
     console.history = console.history || [];
@@ -7,7 +46,7 @@
       for (var arg of arguments) {
         console.history.push({
           time: (new Date()).toString(),
-          value: arg,
+          value: serializable(arg),
           type: 'message' } );
       }
       console.undecorated_log.apply(console, arguments);
@@ -17,7 +56,7 @@
     console.table = function(){
       console.history.push({
         time: (new Date()).toString(),
-        value:  Array.prototype.slice.call(arguments),
+        value:  serializable(Array.prototype.slice.call(arguments)),
         type: 'table'
       });
       console.undecorated_table.apply(console, arguments);
