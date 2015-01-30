f849ade7db7be4a3591893c27275b3b889a6a310
Evan Dorn <evan@lrdesign.com>
2015-01-29 17:52:14 -0800

    Log wrapper now handles both tables and log messages.
    [#87057100]

--- frontend/src/common/log_history.js
+++ frontend/src/common/log_history.js
@@ -5,9 +5,22 @@
     console.history = console.history || [];
     console.log = function(){
       for (var arg of arguments) {
-        console.history.push(arg);
+        console.history.push({
+          time: (new Date()).toString(),
+          value: arg,
+          type: 'message' } );
       }
       console.undecorated_log.apply(console, arguments);
+    };
+
+    console.undecorated_table = console.table;
+    console.table = function(){
+      console.history.push({
+        time: (new Date()).toString(),
+        value:  Array.prototype.slice.call(arguments),
+        type: 'table'
+      });
+      console.undecorated_table.apply(console, arguments);
     };
   }
 })();
