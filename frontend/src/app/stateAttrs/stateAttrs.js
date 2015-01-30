2e7e0bc307e93b9e2faf46e4b7c042d4bca33040
Hannah Howard <hannah@hannahhoward.net>
2015-01-30 14:22:15 -0800

    Modify lrd_state_attrs

--- frontend/src/app/stateAttrs/stateAttrs.js
+++ frontend/src/app/stateAttrs/stateAttrs.js
@@ -1,30 +1,33 @@
 import {} from "../../../build/templates-app";
-import {Module, Directive} from 'a1atscript';
+import {Module, DirectiveObject} from 'a1atscript';
 
 @Module( 'stateAttrs', [ 'templates-app', 'ui.router.state'])
-@Directive('lrdStateAttrs', ['$compile', '$state'])
-export default function lrdStateAttrs($compile, $state) {
-  function getUiViewName(attrs, inherited) {
+@DirectiveObject('lrdStateAttrs', ['$compile', '$state'])
+export default class StateAttrs {
+  constructor($compile, $state) {
+    this.$compile = $compile;
+    this.$state = $state;
+    this.restrict = 'A';
+    this.priority = -500;
+  }
+
+  getUiViewName(attrs, inherited) {
     var name = attrs.uiView || attrs.name || '';
     return name.indexOf('@') >= 0 ?  name :  (name + '@' + (inherited ? inherited.state.name : ''));
   }
 
-  return {
-    restrict: 'A',
-    priority: -500, //uiViewFill is -400
-    link(scope, element, attrs, controller, transcludeFn){
-      var name = getUiViewName(attrs, element.inheritedData('$uiView'));
-      var locals  = name && $state.$current && $state.$current.locals[name];
-      if (locals) {
-        var viewStateName = locals.$$state.self.name;
-        var className = viewStateName.replace(/.*\./,'');
-        var idName = viewStateName.replace(/\./g,'_');
+  link(scope, element, attrs, controller, transcludeFn){
+    var name = this.getUiViewName(attrs, element.inheritedData('$uiView'));
+    var locals  = name && this.$state.$current && this.$state.$current.locals[name];
+    if (locals) {
+      var viewStateName = locals.$$state.self.name;
+      var className = viewStateName.replace(/.*\./,'');
+      var idName = viewStateName.replace(/\./g,'_');
 
-        if(! attrs.id){
-          attrs.$set("id", idName);
-        }
-        attrs.$addClass(className);
+      if(! attrs.id){
+        attrs.$set("id", idName);
       }
+      attrs.$addClass(className);
     }
-  };
+  }
 }
