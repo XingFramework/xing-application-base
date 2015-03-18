fa3362a7968ccbf03c7b1494a228886611d61025
Hannah Howard <hannah@hannahhoward.net>
2015-03-18 12:05:16 -0700

    Use main a1atscript

---
+++ frontend/bower_components/a1atscript/src/a1atscript.js
@@ -0,0 +1,5 @@
+export * from './a1atscript/Injector.js';
+export * from './a1atscript/annotations.js';
+export * from './a1atscript/DirectiveObject.js';
+import './a1atscript/ng2Directives/ComponentInjector.js';
+export * from "./a1atscript/ng2Directives/Component.js";
