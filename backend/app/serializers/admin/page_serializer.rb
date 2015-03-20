b6817e4b6a81ae5fc155bac0db1a8aca89db4d3c
Judson <nyarly@gmail.com>
2015-03-20 14:24:34 -0700

    Admin serializer needs update too

--- backend/app/serializers/admin/page_serializer.rb
+++ backend/app/serializers/admin/page_serializer.rb
@@ -2,6 +2,6 @@ class Admin::PageSerializer < PageSerializer
   attributes :url_slug, :published, :publish_start, :publish_end
 
   def links
-    { :self => routes.admin_page_path(object), :public => routes.page_path(object)  }
+    { :self => routes.admin_page_path(object), :public => routes.page_path(object), :admin => routes.admin_page_path(object) }
   end
 end
