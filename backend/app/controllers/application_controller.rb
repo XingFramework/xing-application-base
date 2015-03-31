7a71d8f9719bd68ccc88737046d20209d9598fc5
Evan Dorn <evan@lrdesign.com>
2015-03-31 11:04:19 -0700

    Move framework controllers and resources serializer into backend/lib/framework.
    [#90532196]

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
-class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
+class ApplicationController < BaseController
 
-  protect_from_forgery
-  before_filter :check_format
-
-  def check_format
-    if request.subdomains.include? BACKEND_SUBDOMAIN
-      if request.headers["Accept"] =~ /json/
-        params[:format] = :json
-      else
-        render :nothing => true, :status => 406
-      end
-    end
-  end
 end
