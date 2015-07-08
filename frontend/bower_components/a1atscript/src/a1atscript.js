7c133ff1acfa14386c4cc35fadec91e26e7b9818
Hannah Howard <hannah@hannahhoward.net>
2015-07-08 16:21:38 -0700

    Remove a1atscript from bower

--- frontend/bower_components/a1atscript/src/a1atscript.js
+++
@@ -1,5 +0,0 @@
-export * from './a1atscript/Injector.js';
-export * from './a1atscript/annotations.js';
-export * from './a1atscript/DirectiveObject.js';
-import './a1atscript/ng2Directives/ComponentInjector.js';
-export * from "./a1atscript/ng2Directives/Component.js";
--- frontend/bower_components/a1atscript/src/a1atscript/ng2Directives/Ng2Directive.js
+++
@@ -1,9 +0,0 @@
-export default class Ng2Directive {
-  constructor(descriptor) {
-    this.selector = descriptor.selector;
-    this.bind = descriptor.bind;
-    this.controllerAs = descriptor.controllerAs;
-    this.require = descriptor.require;
-    this.transclude = descriptor.transclude;
-  }
-}
