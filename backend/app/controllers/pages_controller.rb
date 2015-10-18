17b84054fb22d8f6cadfd28dfcd2e521c296fd60
Evan Dorn <evan@lrdesign.com>
2015-10-18 16:44:35 -0700

    Deleted all the un-needed CMS stuff.
    [#105521786]

--- backend/app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-  respond_to :json
-
-  def show
-    path = params[:url_slug]
-    @page = Page.find_by_url_slug(path)
-    render :json => PageSerializer.new(@page)
-  end
-end
--- backend/app/serializers/content_block_serializer.rb
+++
@@ -1,7 +0,0 @@
-class ContentBlockSerializer < Xing::Serializers::Base
-  attributes :content_type, :body
-
-  def links
-    { :self => routes.admin_content_block_path(object) }
-  end
-end
