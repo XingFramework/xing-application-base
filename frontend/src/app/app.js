cddd5d27806e7bc411244921ddf4b0bd05e9555c
Hannah Howard <hannah@hannahhoward.net>
2015-10-18 23:31:36 -0700

    Complete extraction of xing-frontend-utils

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -1,21 +1,23 @@
 import {appName} from 'config';
 import {} from 'build/templates-app.js';
 import {} from 'build/templates-common.js';
-import StateAttrs from 'framework/stateAttrs.js';
-import UIRouteLogger from "framework/ui-route-logger.js";
-import StateFallback from 'framework/stateFallback/stateFallback.js';
 
 import { Module, Injector } from "a1atscript";
-
-import Toast from 'components/toast/toast.js';
-import ResponsiveMenu from 'components/responsiveMenu/responsiveMenu.js';
 import SessionLinks from 'components/sessionLinks/sessionLinks.js';
-import XngUnimplementedDirective from 'components/unimplemented/unimplemented.js'
 
-import Fallback from './fallback/fallback.js'
+import {
+  Fallback,
+  Toast,
+  ResponsiveMenu,
+  UnimplementedDirective,
+  StateAttrs,
+  uiRouteLogger,
+  stateFallback,
+  ExampleForm,
+} from 'xing-frontend-utils';
+
 import Auth from './auth/auth.js';
 import Homepage from './homepage/homepage.js';
-import ExampleForm from './exampleForm/exampleForm.js';
 
 import * as appConfig from './appConfig.js';
 import RootCtrl from './rootController.js';
@@ -24,13 +26,13 @@ var app = new Module(appName, [
   'templates-app', 'templates-common', 'ui.router',
   'picardy.fontawesome',
   StateAttrs,
-  UIRouteLogger,
-  StateFallback,
+  uiRouteLogger,
+  stateFallback,
   Homepage,
   Auth,
   Fallback,
   ResponsiveMenu,
-  XngUnimplementedDirective,
+  UnimplementedDirective,
   ExampleForm,
   SessionLinks,
   Toast,
