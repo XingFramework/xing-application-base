60c9feab4c5a48f02e4565ad7b7f08fff8e4acd0
Judson <nyarly@gmail.com>
2015-10-18 20:00:14 -0700

    Replaced local build config with xing-frontend-grunt

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,31 +1,3 @@
 module.exports = function( grunt ) {
   require('xing-frontend-grunt')(grunt);
-
-  /**
-   * Load in our build configuration file.
-   */
-  var userConfig = require( './build.config.js' );
-
-  userConfig.pkg = grunt.file.readJSON("package.json");
-
-  var liveReloadPort   = require('./grunt/support/liveReloadPort.js');
-  var karmaRunnerPort  = require('./grunt/support/karmaRunnerPort.js');
-
-  grunt.cacheMap = []
-
-  var path = require('path');
-
-  require('load-grunt-config')(grunt, {
-    configPath: path.join(process.cwd(), 'grunt/configs')
-  });
-
-  /**
-   * This is the configuration object Grunt uses to give each plugin its
-   * instructions.
-   */
-
-  grunt.config.merge( userConfig );
-
-  grunt.loadTasks('grunt/tasks');
-
 };
