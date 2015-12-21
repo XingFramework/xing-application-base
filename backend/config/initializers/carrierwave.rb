50dd810751dbe8c078da61a58f53124750b130e9
patriciaho <patricia.p.ho@gmail.com>
2015-12-21 14:19:16 -0800

    remove unused code

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,10 +1,3 @@
-# CarrierWave.configure do |config|
-#   config.asset_host = proc do |file|
-#     identifier = # some logic
-#     "http://#{identifier}.cdn.rackspacecloud.com"
-#   end
-# end
-
 CarrierWave.configure do |config|
   config.storage = :file
   config.asset_host = ActionController::Base.asset_host
