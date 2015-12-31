48a4d7ef4cb16b6d441fda37dc506d9cdb810961
Judson <nyarly@gmail.com>
2015-12-30 18:31:15 -0800

    Stripped CMS constants

--- backend/config/initializers/constants.rb
+++ backend/config/initializers/constants.rb
@@ -1,18 +1,1 @@
-STATIC_PATHS_FOR_SITEMAP = [
-  '',
-  'sign-in'
-]
-
-PAGE_LAYOUTS = {
-  "Default" => nil,
-  "Blog" => "blog"
-}
-
-NAV_MENU_CACHE = "nav_menu"
-
-NAV_TEMPLATE_NAMES = {
-  :nav => {:node => "shared/nav_node", :list => "shared/nav_list"}
-}
-
 BACKEND_SUBDOMAIN = "api"
-PAGES_FRONTEND_URL = "pages"
