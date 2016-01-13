b7346c370c6bd7e88722cd1e6910952cd5bf8c38
Judson <nyarly@gmail.com>
2016-01-12 17:23:27 -0800

    Little picky tweaks

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_xng_base_session'
+Rails.application.config.session_store :cookie_store, key: '_xing_base_session'
